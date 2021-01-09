f7e6b9fb763d8df0ad77d1728b44ea06db436ea7
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-01-09 17:09:56 +0200

    Show the list

--- OdeFood/Pages/Restruants/List.cshtml.cs
+++ OdeFood/Pages/Restruants/List.cshtml.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using System.Collections.Generic;
 
 namespace OdeFood
 {
     public class ListModel : PageModel
     {
         private IConfiguration _confg;
-        public ListModel(IConfiguration confg)
+        private IRestaurantData _restaurantData;
+        public IEnumerable<Restaurant> restaurant;
+        public ListModel(IConfiguration confg, IRestaurantData restaurantData)
         {
             _confg = confg;
+            _restaurantData = restaurantData;
         }
         public string Message { get; set; }
         public void OnGet()
         {
             Message = _confg["Message"];
+            restaurant = _restaurantData.Getall();
         }
     }
 }
