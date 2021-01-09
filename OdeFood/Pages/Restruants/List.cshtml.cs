e85e4fffd646b402adef331b0d67c75d27c36418
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-01-09 16:05:44 +0200

    show message form appsettings

---
+++ OdeFood/Pages/Restruants/List.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeFood
+{
+    public class ListModel : PageModel
+    {
+        private IConfiguration _confg;
+        public ListModel(IConfiguration confg)
+        {
+            _confg = confg;
+        }
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = _confg["Message"];
+        }
+    }
+}
