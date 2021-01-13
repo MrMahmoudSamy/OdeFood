cdd3fa5ef71d5fcabe3f46399fd13e4bd10ac36c
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-01-13 15:46:50 +0200

    Create Database

--- OdeToFood.Data/OdeToFoodContext.cs
+++ OdeToFood.Data/OdeToFoodContext.cs
@@ -5,6 +5,11 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodContext :DbContext
     {
+        public OdeToFoodContext(DbContextOptions<OdeToFoodContext> options)
+        :base(options)
+        {
+
+        }
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
