69a2c88bfcdf714a2582c40f19c4451190251fa0
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-01-16 17:15:21 +0200

    Adding Delete

---
+++ OdeFood/Pages/Restruants/Delete.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeFood
+{
+    public class DeleteModel : PageModel
+    {
+        private IRestaurantData _restaurantData;
+        public Restaurant Restaurant { get; set; }
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetRestaurantById(restaurantId);
+            if(Restaurant==null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restturant = _restaurantData.Delete(restaurantId);
+            _restaurantData.Commit();
+            if(restturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            TempData["Message"] = $"{restturant.Name} is deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
